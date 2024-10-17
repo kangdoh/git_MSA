2c8b90802ff16418cf0f1c6c6eef1fc72102fd1d
DESKTOP-SCJ83VA\user <khj754211@gmail.com>
2024-10-17 13:03:41 +0900

    no message

---
+++ vue_WORK/user-todo-dayjs/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Home view 입니다.</h1>
+
+    <h1>{{ now.format('YYYY/MM/DD') }}</h1>
+    <hr>
+    <h2>- 빼기 -</h2>
+    <h1>년 빼기 : {{ now.subtract(1,'year').format('YYYY/MM/DD') }}</h1>
+    <h1>월 빼기 : {{ now.subtract(1,'month').format('YYYY/MM/DD') }}</h1>
+    <h1>일 빼기 : {{ now.subtract(1,'day').format('YYYY/MM/DD') }}</h1>
+    <hr>
+    <h2>- 더하기 -</h2>
+    <h1>년 더하기 : {{ now.add(1,'year').format('YYYY/MM/DD') }}</h1>
+    <h1>월 더하기 : {{ now.add(1,'month').format('YYYY/MM/DD') }}</h1>
+    <h1>일 더하기 : {{ now.add(1,'day').format('YYYY/MM/DD') }}</h1>
+    <hr>
+    <h1>이번달 기준</h1>
+    <h1>첫째날 : {{ now.startOf('month') }}</h1>
+    <h1>마지막날 : {{ now.endOf('month') }}</h1>
+    
+  </div>
+</template>
+
+<script setup>
+ import dayjs from 'dayjs';
+  const now = new dayjs();
+
+  
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_WORK/user-todo-dayjs/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+}
+
